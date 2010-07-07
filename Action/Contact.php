12fc92b861bab4f5ba601d471ddc6660b55c56a9
Trevor Morse <trevor@trevor-laptop.(none)>
2010-07-06 21:53:47 -0300

    Adding Contact action

---
+++ Action/Contact.php
@@ -0,0 +1,189 @@
+<?php
+
+/**
+ * Action Contact
+ *
+ * Handles request for contacts.
+ *
+ * @link http://getfrapi.com
+ * @author Frapi <frapi@getfrapi.com>
+ * @link /contacts/:contact_id
+ */
+class Action_Contact extends Frapi_Action implements Frapi_Action_Interface
+{
+
+    /**
+     * Required parameters
+     *
+     * @var An array of required parameters.
+     */
+    protected $requiredParams = array(
+        'username',
+        'password'
+        );
+
+    /**
+     * The data container to use in toArray()
+     *
+     * @var A container of data to fill and return in toArray()
+     */
+    private $data = array();
+
+    /**
+     * To Array
+     *
+     * This method returns the value found in the database
+     * into an associative array.
+     *
+     * @return array
+     */
+    public function toArray()
+    {
+        $this->data['username'] = $this->getParam('username', self::TYPE_OUTPUT);
+        $this->data['password'] = $this->getParam('password', self::TYPE_OUTPUT);
+        return $this->data;
+    }
+
+    /**
+     * Default Call Method
+     *
+     * This method is called when no specific request handler has been found
+     *
+     * @return array
+     */
+    public function executeAction()
+    {
+        $valid = $this->hasRequiredParameters($this->requiredParams);
+        if ($valid instanceof Frapi_Error) {
+            return $valid;
+        }
+
+        return $this->toArray();
+    }
+
+    /**
+     * Get Request Handler
+     *
+     * This method is called when a request is a GET
+     *
+     * @return array
+     */
+    public function executeGet()
+    {
+        $valid = $this->hasRequiredParameters($this->requiredParams);
+        if ($valid instanceof Frapi_Error) {
+            return $valid;
+        }
+
+        $username   = $this->getParam('username');
+        $password   = $this->getParam('password');
+        $contact_id = $this->getParam('contact_id', self::TYPE_INT);
+
+        // Attempt to login as user, a little bit of a hack as we currently
+        // require the $_POST['login'] var to be set as well as a global AppUI
+        $AppUI              = new CAppUI();
+        $GLOBALS['AppUI']   = $AppUI;
+        $_POST['login']     = 'login';
+
+        if (!$AppUI->login($username, $password)) {
+            throw new Frapi_Error('INVALID_LOGIN');
+        }
+
+        $contact = new CContact();
+        $allowed_contacts = $contact->getAllowedRecords($AppUI->user_id);
+
+        // Contact ID is the key, so lets get them in an array so we can
+        // easily check
+        $allowed_contacts = array_keys($allowed_contacts);
+
+        if (!in_array($contact_id, $allowed_contacts)) {
+            throw new Frapi_Error('PERMISSION_ERROR');
+        }
+
+        // User has permission so load the contact for display
+        $contact_array                      = (array)$contact->load($contact_id);
+        $contact_array['contact_methods']   = $contact->getContactMethods();
+
+        // Remove the data that is not for display
+        unset(
+            $contact_array['tbl_prefix'], $contact_array['_tbl'], $contact_array['_tbl_key'],
+            $contact_array['_error'], $contact_array['_query']
+        );
+
+        $this->data['contact'] = $contact_array;
+        $this->data['success'] = true;
+
+        return $this->toArray();
+    }
+
+    /**
+     * Post Request Handler
+     *
+     * This method is called when a request is a POST
+     *
+     * @return array
+     */
+    public function executePost()
+    {
+        $valid = $this->hasRequiredParameters($this->requiredParams);
+        if ($valid instanceof Frapi_Error) {
+            return $valid;
+        }
+
+        return $this->toArray();
+    }
+
+    /**
+     * Put Request Handler
+     *
+     * This method is called when a request is a PUT
+     *
+     * @return array
+     */
+    public function executePut()
+    {
+        $valid = $this->hasRequiredParameters($this->requiredParams);
+        if ($valid instanceof Frapi_Error) {
+            return $valid;
+        }
+
+        return $this->toArray();
+    }
+
+    /**
+     * Delete Request Handler
+     *
+     * This method is called when a request is a DELETE
+     *
+     * @return array
+     */
+    public function executeDelete()
+    {
+        $valid = $this->hasRequiredParameters($this->requiredParams);
+        if ($valid instanceof Frapi_Error) {
+            return $valid;
+        }
+
+        return $this->toArray();
+    }
+
+    /**
+     * Head Request Handler
+     *
+     * This method is called when a request is a HEAD
+     *
+     * @return array
+     */
+    public function executeHead()
+    {
+        $valid = $this->hasRequiredParameters($this->requiredParams);
+        if ($valid instanceof Frapi_Error) {
+            return $valid;
+        }
+
+        return $this->toArray();
+    }
+
+
+}
+
