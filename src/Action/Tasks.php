dad8cfeefbb91c4ab2e10e5dbcb85db8152dd099
trevormorse <trevor.morse@gmail.com>
2011-02-08 20:43:18 -0400

    Adding put action for Tasks

--- src/Action/Tasks.php
+++ src/Action/Tasks.php
@@ -135,12 +135,239 @@ class Action_Tasks extends Frapi_Action implements Frapi_Action_Interface
      */
     public function executePut()
     {
+        /**
+         * @todo Remove this once we figure out how to reference vars in file
+         * that is autoloaded
+         */
+        global $tracking_dynamics;
+
         $valid = $this->hasRequiredParameters($this->requiredParams);
         if ($valid instanceof Frapi_Error) {
             return $valid;
         }
 
-        return $this->toArray();
+        $username        = $this->getParam('username');
+        $password        = $this->getParam('password');
+        $hassign         = $this->getParam('hassign');
+        $hdependencies   = $this->getParam('hdependencies');
+        $notify          = $this->getParam('task_notify');
+        $comment         = $this->getParam('email_comment');
+        $task_id         = $this->getParam('task_id');
+        $adjustStartDate = $this->getParam('set_task_start_date');
+
+        // Attempt to login as user, a little bit of a hack as we currently
+        // require the $_POST['login'] var to be set as well as a global AppUI
+        $AppUI              = new CAppUI();
+        $GLOBALS['AppUI']   = $AppUI;
+        $_POST['login']     = 'login';
+
+        if (!$AppUI->login($username, $password)) {
+            throw new Frapi_Error('INVALID_LOGIN');
+        }
+
+        $post_data = array(
+            'task_id'                         => 0,
+            'task_name'                       => $this->getParam('task_name'),
+            'task_status'                     => $this->getParam('task_status'),
+            'task_percent_complete'           => $this->getParam('task_percent_complete'),
+            'task_milestone'                  => $this->getParam('task_milestone'),
+            'task_owner'                      => $this->getParam('task_owner'),
+            'task_access'                     => $this->getParam('task_access'),
+            'task_related_url'                => $this->getParam('task_related_url'),
+            'task_parent'                     => $this->getParam('task_parent'),
+            'task_type'                       => $this->getParam('task_type'),
+            'task_target_budget'              => $this->getParam('task_target_budget'),
+            'task_description'                => $this->getParam('task_description'),
+            'task_start_date'                 => $this->getParam('task_start_date'),
+            'task_end_date'                   => $this->getParam('task_end_date'),
+            'task_duration'                   => $this->getParam('task_duration'),
+            'task_duration_type'              => $this->getParam('task_duration_type'),
+            'task_dynamic'                    => $this->getParam('task_dynamic'),
+            'task_allow_other_user_tasklogs' => $this->getParam('task_allow_other_user_tasklogs'),
+            'task_project'                    => $this->getParam('task_project'),
+            'task_priority'                   => $this->getParam('task_priority'),
+        );
+
+        // Include any files for handling module-specific requirements
+        foreach (findTabModules('tasks', 'addedit') as $mod) {
+            $fname = W2P_BASE_DIR . '/modules/' . $mod . '/tasks_dosql.addedit.php';
+            if (file_exists($fname)) {
+                require_once $fname;
+            }
+        }
+
+        // Find the task if we are set
+        $task_end_date = null;
+        if ($task_id) {
+            $task->load($task_id);
+            $task_end_date = new w2p_Utilities_Date($task->task_end_date);
+        }
+
+        $task = new CTask();
+
+        if(!$task->bind($post_data)) {
+            throw new Frapi_Error('SAVE_ERROR', $task->getError());
+        }
+
+        if ($task->task_dynamic != 1) {
+            $task_dynamic_delay = ($this->getParam('task_dynamic_nodelay')) ? $this->getParam('task_dynamic_nodelay') : '0';
+            if (in_array($task->task_dynamic, $tracking_dynamics)) {
+                $task->task_dynamic = $task_dynamic_delay ? 21 : 31;
+            } else {
+                $task->task_dynamic = $task_dynamic_delay ? 11 : 0;
+            }
+        }
+
+        // Let's check if task_dynamic is unchecked
+        if (!$this->getParam('task_dynamic')) {
+            $task->task_dynamic = false;
+        }
+
+        // Make sure task milestone is set or reset as appropriate
+        if ($this->getParam('task_milestone')) {
+            $task->task_milestone = false;
+        }
+
+        //format hperc_assign user_id=percentage_assignment;user_id=percentage_assignment;user_id=percentage_assignment;
+        $tmp_ar = explode(';', $this->getParam('hperc_assign'));
+        $i_cmp = sizeof($tmp_ar);
+
+        $hperc_assign_ar = array();
+        for ($i = 0; $i < $i_cmp; $i++) {
+            $tmp = explode('=', $tmp_ar[$i]);
+            if (count($tmp) > 1) {
+                $hperc_assign_ar[$tmp[0]] = $tmp[1];
+            } elseif ($tmp[0] != '') {
+                $hperc_assign_ar[$tmp[0]] = 100;
+            }
+        }
+
+        // let's check if there are some assigned departments to task
+        $task->task_departments = implode(',', $this->getParam('dept_ids', self::TYPE_ARRAY));
+
+        // convert dates to SQL format first
+        if ($task->task_start_date) {
+            $date = new w2p_Utilities_Date($task->task_start_date);
+            $task->task_start_date = $date->format(FMT_DATETIME_MYSQL);
+        }
+        $end_date = null;
+        if ($task->task_end_date) {
+            if (strpos($task->task_end_date, '2400') !== false) {
+                $task->task_end_date = str_replace('2400', '2359', $task->task_end_date);
+            }
+            $end_date = new w2p_Utilities_Date($task->task_end_date);
+            $task->task_end_date = $end_date->format(FMT_DATETIME_MYSQL);
+        }
+
+        $error_array = $task->store($AppUI);
+
+        // Return all the validation messages
+        if ($error_array !== true) {
+            $error_message = '';
+            foreach ($error_array as $error) {
+                $error_message .= $error . '. ';
+            }
+
+            throw new Frapi_Error('SAVE_ERROR', $error_message);
+        }
+
+        $task_parent     = ($this->getParam('task_parent')) ? $this->getParam('task_parent', SELF::TYPE_INT) : 0;
+        $old_task_parent = ($this->getParam('old_task_parent')) ? $this->getParam('old_task_parent', SELF::TYPE_INT) : 0;
+
+        if ($task_parent != $old_task_parent) {
+            $oldTask = new CTask();
+            $oldTask->load($old_task_parent);
+            $oldTask->updateDynamics(false);
+        }
+
+        // How to handle custom fields? Do we support it in api?
+
+        // Now add any task reminders
+        // If there wasn't a task, but there is one now, and
+        // that task date is set, we need to set a reminder.
+        if (empty($task_end_date) || (!empty($end_date) && $task_end_date->dateDiff($end_date))) {
+            $task->addReminder();
+        }
+
+        if (isset($hassign)) {
+            $task->updateAssigned($hassign, $hperc_assign_ar);
+        }
+
+        if (isset($hdependencies)) { // && !empty($hdependencies)) {
+                // there are dependencies set!
+
+                // backup initial start and end dates
+                $tsd = new w2p_Utilities_Date($task->task_start_date);
+                $ted = new w2p_Utilities_Date($task->task_end_date);
+
+                // updating the table recording the
+                // dependency relations with this task
+                $task->updateDependencies($hdependencies, $task_parent);
+
+                // we will reset the task's start date based upon dependencies
+                // and shift the end date appropriately
+                if ($adjustStartDate && !is_null($hdependencies)) {
+
+                // load already stored task data for this task
+                $tempTask = new CTask();
+                $tempTask->load($task->task_id);
+
+                // shift new start date to the last dependency end date
+                $nsd = new w2p_Utilities_Date($tempTask->get_deps_max_end_date($tempTask));
+
+                // prefer Wed 8:00 over Tue 16:00 as start date
+                $nsd = $nsd->next_working_day();
+
+                // prepare the creation of the end date
+                $ned = new w2p_Utilities_Date();
+                $ned->copy($nsd);
+
+                if (empty($task->task_start_date)) {
+                    // appropriately calculated end date via start+duration
+                    $ned->addDuration($task->task_duration, $task->task_duration_type);
+
+                } else {
+                    // calc task time span start - end
+                    $d = $tsd->calcDuration($ted);
+
+                    // Re-add (keep) task time span for end date.
+                    // This is independent from $obj->task_duration.
+                    // The value returned by Date::Duration() is always in hours ('1')
+                    $ned->addDuration($d, '1');
+
+                }
+
+                // prefer tue 16:00 over wed 8:00 as an end date
+                $ned = $ned->prev_working_day();
+
+                $task->task_start_date = $nsd->format(FMT_DATETIME_MYSQL);
+                $task->task_end_date = $ned->format(FMT_DATETIME_MYSQL);
+
+                $q = new w2p_Database_Query;
+                $q->addTable('tasks', 't');
+                $q->addUpdate('task_start_date', $task->task_start_date);
+                $q->addUpdate('task_end_date', $task->task_end_date);
+                $q->addWhere('task_id = ' . (int)$task->task_id);
+                $q->addWhere('task_dynamic <> 1');
+                $q->exec();
+                $q->clear();
+                }
+                $task->pushDependencies($task->task_id, $task->task_end_date);
+        }
+
+        unset(
+            $task->_query, $task->_error, $task->_tbl_prefix,
+            $task->_tbl, $task->_tbl_key, $task->_tbl_module
+        );
+        $task = (array)$task;
+
+        $this->data['task'] = $task;
+        $this->data['success'] = true;
+
+        return new Frapi_Response(array(
+            'code' => 201,
+            'data' => $this->data
+        ));
     }
 
     /**
--- src/AllFiles.php
+++ src/AllFiles.php
@@ -21,3 +21,17 @@ require W2P_INSTALL_DIR . DIRECTORY_SEPARATOR . 'base.php';
 require W2P_BASE_DIR . W2P_INCLUDE_DIR . 'config.php';
 require W2P_BASE_DIR . W2P_INCLUDE_DIR . 'main_functions.php';
 require W2P_BASE_DIR . W2P_INCLUDE_DIR . 'db_adodb.php';
+
+/*
+ * This is absolutely disgusting, but there doesn't seem to be away to get
+ * these vars any other way in the methods.
+ *
+ * @todo Figure out how to get the variables defined in an autoload class file
+ * working and remove this.
+ */
+// When calculating a task's start date only consider
+// end dates of tasks with these dynamic values.
+$tracked_dynamics = array('0' => '0', '1' => '1', '2' => '31');
+// Tasks with these dynamics have their dates updated when
+// one of their dependencies changes. (They track dependencies)
+$tracking_dynamics = array('0' => '21', '1' => '31');
