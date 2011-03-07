8ff932dbae876aa4f8a9b231e0dce349719cb6ce
trevormorse <trevor.morse@gmail.com>
2011-03-06 21:34:00 -0400

    Adding POST for task action

--- tests/tasks.test.php
+++ tests/tasks.test.php
@@ -317,4 +317,135 @@ class Tasks_Test extends Test_Base {
         $this->assertEquals('SAVE_ERROR',   $body->errors[0]->name);
         $this->assertEquals('',             $body->errors[0]->at);
     }
+
+    /**
+     *  Testing a post
+     *
+     * @access public
+     *
+     * @return void
+     */
+    public function testPostJSON()
+    {
+        $this->post_data = array (
+            'task_name'                      => '*API* Task Name Updated',
+            'task_status'                    => 1,
+            'task_percent_complete'          => 60,
+            'task_milestone'                 => 0,
+            'task_owner'                     => 1,
+            'task_access'                    => 2,
+            'task_related_url'               => 'http://updated.api.example.org',
+            'task_parent'                    => 0,
+            'task_type'                      => 1,
+            'task_target_budget'             => 600.00,
+            'task_description'               => '*API* Task Description Updated',
+            'task_start_date'                => '2011-03-06 11:00:00',
+            'task_end_date'                  => '2011-03-10 11:00:00',
+            'task_duration'                  => 4,
+            'task_duration_type'             => 24,
+            'task_dynamic'                   => 0,
+            'task_allow_other_user_tasklogs' => 1,
+            'task_project'                   => 1,
+            'task_priority'                  => 1,
+            'task_notify'                    => 1,
+            'dept_ids'                       => array(1),
+            'hperc_assign'                   => '1;100'
+        );
+
+        $result     = parent::makeRequest('task', array($this->task_id), 'POST', $this->post_data);
+        $headers    = $result->getHeader();
+        $body       = json_decode($result->getBody());
+
+        $this->assertEquals(200,                                $result->getStatus());
+        $this->assertEquals('OK',                               $result->getReasonPhrase());
+        $this->assertEquals('application/json; charset=utf-8',  $headers['content-type']);
+        $task = $body->task;
+
+        $this->assertEquals($this->task_id,                                            $task->task_id);
+        $this->assertEquals(31,                                                        count(get_object_vars($task)));
+        $this->assertEquals('*API* Task Name Updated',                                 $task->task_name);
+        $this->assertEquals($this->task_id,                                            $task->task_parent);
+        $this->assertEquals(0,                                                         $task->task_milestone);
+        $this->assertEquals(1,                                                         $task->task_project);
+        $this->assertEquals(1,                                                         $task->task_owner);
+        $this->assertEquals('2011-03-06 16:00:00',                                     $task->task_start_date);
+        $this->assertEquals(4,                                                         $task->task_duration);
+        $this->assertEquals(24,                                                        $task->task_duration_type);
+        $this->assertEquals(0,                                                         $task->task_hours_worked);
+        $this->assertEquals('2011-03-10 16:00:00',                                     $task->task_end_date);
+        $this->assertEquals(1,                                                         $task->task_status);
+        $this->assertEquals(1,                                                         $task->task_priority);
+        $this->assertEquals(60,                                                        $task->task_percent_complete);
+        $this->assertEquals('*API* Task Description Updated',                          $task->task_description);
+        $this->assertEquals(600,                                                       $task->task_target_budget);
+        $this->assertEquals('http://updated.api.example.org',                          $task->task_related_url);
+        $this->assertEquals(1,                                                         $task->task_creator);
+        $this->assertEquals(0,                                                         $task->task_order);
+        $this->assertEquals(0,                                                         $task->task_client_publish);
+        $this->assertEquals(0,                                                         $task->task_dynamic);
+        $this->assertEquals(2,                                                         $task->task_access);
+        $this->assertEquals(0,                                                         $task->task_notify);
+        $this->assertEquals(1,                                                         $task->task_departments);
+        $this->assertEquals('',                                                        $task->task_contacts);
+        $this->assertEquals('',                                                        $task->task_custom);
+        $this->assertEquals(1,                                                         $task->task_type);
+        $this->assertRegExp('/[0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2}/', $task->task_created);
+        $this->assertRegExp('/[0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2}/', $task->task_updated);
+        $this->assertGreaterThanOrEqual(time() - 10,                                   strtotime($task->task_created));
+        $this->assertGreaterThanOrEqual(time() - 10,                                   strtotime($task->task_updated));
+        $this->assertEquals(0,                                                         $task->task_updator);
+        $this->assertEquals(1,                                                         $task->task_allow_other_user_tasklogs);
+        $this->assertTrue($body->success);
+    }
+
+    /**
+     * Testing a post with invalid login
+     *
+     * @access public
+     *
+     * @return void
+     */
+    public function testPostInvalidLoginJSON()
+    {
+        $result     = parent::makeRequest('task', array($this->task_id), 'POST', null, array('username' => '', 'password' => ''));
+        $headers    = $result->getHeader();
+        $body       = json_decode($result->getBody());
+
+        $this->assertEquals(401,                                $result->getStatus());
+        $this->assertEquals('Authorization Required',           $result->getReasonPhrase());
+        $this->assertEquals('application/json; charset=utf-8',  $headers['content-type']);
+        $this->assertEquals('Invalid Username or Password.',    $body->errors[0]->message);
+        $this->assertEquals('INVALID_LOGIN',                    $body->errors[0]->name);
+        $this->assertEquals('',                                 $body->errors[0]->at);
+    }
+
+    /**
+     * Testing a post with missing parameters
+     *
+     * @access public
+     *
+     * @return void
+     */
+    public function testPostIvalidParamsJSON()
+    {
+        unset(
+            $this->post_data['task_name'], $this->post_data['task_project']
+        );
+
+        $result     = parent::makeRequest('task', array($this->task_id), 'POST',  $this->post_data);
+
+        $headers    = $result->getHeader();
+        $body       = json_decode($result->getBody());
+
+        $this->assertEquals(401,                                $result->getStatus());
+        $this->assertEquals('Authorization Required',           $result->getReasonPhrase());
+        $this->assertEquals('application/json; charset=utf-8',  $headers['content-type']);
+
+        $this->assertEquals(
+            'CTask::store-check failed - task name is NULL. CTask::store-check failed - task project is not set. ',
+            $body->errors[0]->message
+        );
+        $this->assertEquals('SAVE_ERROR',   $body->errors[0]->name);
+        $this->assertEquals('',             $body->errors[0]->at);
+    }
 }
